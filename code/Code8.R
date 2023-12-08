404d9a031e6ca94593a49d0e8b4b070bc9c7f0c5
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2023-12-08 09:34:24 -0600

    Day 8 part 2. I think it works?

--- code/Code8.R
+++ code/Code8.R
@@ -1,4 +1,48 @@
 directions <- c("LRRLRRRLRRLRRLRRRLRRLRLLRRRLRRRLRRRLRRRLRRRLRRLLRRRLLRLRRRLRRLRRRLLRRRLRLLRRLRLLRLRLLRRLRRRLRRLLRRRLRRRLRLRRRLRRRLRRRLRRRLRLRRRLLRRRLRRLRRRLRLRRRLRRLRLLLLLRRRLRRRLRRRLRRRLRRLLRLRLRRLRRLLRRRLRRRLRRRLLLRRRLRRRLRRRLRLRRRLLRLRLRRLRRLRRRLRRLRRRLRRRLRRRLRRLLRRRLRRLRRLRLLRRRR")
+directions <- strsplit(directions,"")[[1]]
+
+dat <- read.delim("/Users/gregorymatthews/Dropbox/AOC2023/data/data8.txt", sep = " ", header = FALSE)
+dat$V3 <- gsub("(\\(|\\)|,)","",dat$V3)
+dat$V4 <- gsub("(\\(|\\))","",dat$V4)
+dat <- dat[,c(1,3,4)]
+names(dat) <- c("A","L","R")
+
+#Start at AAA
+i <- 1
+loc <- "AAA"
+nexttt <- function(loc,i){
+dir <- directions[i]
+return(dat[dat$A == loc,dir])
+}
+count <- 0
+loc <- "AAA"
+i <- 1
+while (loc != "ZZZ"){
+  loc <- nexttt(loc,i)
+  count <- count + 1
+  if (i < 269){i <- i + 1} else {i <- 1}
+  print(loc)
+
+}
 
 
-read.delim("/Users/gregorymatthews/Dropbox/AOC2023/data/data5_seed2soil.txt", sep = " ", header = FALSE)
+#Step 2
+nexttt <- function(loc,i){
+  dir <- directions[i]
+  return(dat[dat$A %in% loc,dir])
+}
+
+start <- dat$A[substring(dat$A,3,3) == "A"]
+
+count <- 0
+loc <- start
+i <- 1
+while (any(substring(loc,3,3) != "Z")){
+  loc <- nexttt(loc,i)
+  count <- count + 1
+  if (i < 269){i <- i + 1} else {i <- 1}
+  print(count)
+}
+count
+
+

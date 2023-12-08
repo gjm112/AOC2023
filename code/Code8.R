2ecd373685b83d25d5a35e99740462d65f6eba38
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2023-12-08 17:47:17 -0600

    code 8.

--- code/Code8.R
+++ code/Code8.R
@@ -11,8 +11,8 @@ names(dat) <- c("A","L","R")
 i <- 1
 loc <- "AAA"
 nexttt <- function(loc,i){
-dir <- directions[i]
-return(dat[dat$A == loc,dir])
+  dir <- directions[i]
+  return(dat[dat$A == loc,dir])
 }
 count <- 0
 loc <- "AAA"
@@ -22,8 +22,37 @@ while (loc != "ZZZ"){
   count <- count + 1
   if (i < 269){i <- i + 1} else {i <- 1}
   print(loc)
-
+  
 }
+
+QXA, AAA, PDA, TDA, QQA, PPA, 
+c(12643, 16409, 14257, 15871, 18023, 19637)/269
+count <- 0
+loc <- "QXA"
+i <- 1
+while (!grepl("..Z",loc)){
+  loc <- nexttt(loc,i)
+  count <- count + 1
+  if (i < 269){i <- i + 1} else {i <- 1}
+  print(loc)
+  
+}
+print(count)
+
+library(pracma)
+pracma::Lcm(12643, 16409)
+pracma::Lcm(771223, 16409) 
+pracma::Lcm(771223,  14257) 
+pracma::Lcm(40874819,  15871) 
+pracma::Lcm(2411614321     ,  18023) 
+library(gmp)
+as.bigz(pracma::Lcm(161578159507,19637))
+
+
+
+
+
+
 
 
 #Step 2
@@ -41,7 +70,7 @@ while (any(substring(loc,3,3) != "Z")){
   loc <- nexttt(loc,i)
   count <- count + 1
   if (i < 269){i <- i + 1} else {i <- 1}
-  print(count)
+  if (count %% 100000 == 0){print(count)}
 }
 count
 

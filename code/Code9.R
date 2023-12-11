28dabd745f8c97e4d1b876750bb0699b04aaf525
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2023-12-11 12:56:59 -0600

    Code9 finsihed.  Sometimes I make really stupid mistakes.

--- code/Code9.R
+++ code/Code9.R
@@ -1,21 +1,49 @@
 dat <- read.delim("/Users/gregorymatthews/Dropbox/AOC2023/data/data9.txt", sep = " ", header = FALSE)
 dat <- as.matrix(dat)
-x <- dat[1,]
-# dat <- 
-# rbind(c(0, 3, 6, 9, 12, 15),
-# c(1, 3, 6, 10, 15, 21),
-# c(10, 13, 16, 21, 30, 45))
+#dat <- rbind(c(0, 3, 6, 9, 12, 15),c(1, 3, 6, 10, 15, 21),c(10, 13, 16, 21, 30, 45))
+# x <- dat[1,]
 getends <- function(x){
+end <- c()
 temp <- x
-while (any(temp > 0)){
+while (!all(temp == 0)){
 print(temp)
 end <- c(end,temp[length(temp)])
 temp <- diff(temp)
 }
+
 return(sum(end))
 }
-
 lll <- apply(dat,1,getends)
 sum(lll)
+
+1479039823
+
+
+#Part 3
+dat <- read.delim("/Users/gregorymatthews/Dropbox/AOC2023/data/data9.txt", sep = " ", header = FALSE)
+dat <- as.matrix(dat)
+#dat <- rbind(c(0, 3, 6, 9, 12, 15),c(1, 3, 6, 10, 15, 21),c(10, 13, 16, 21, 30, 45))
+# x <- dat[1,]
+getbegin <- function(x){
+  start <- c()
+  #temp <- c(10,13,16,21,30,45)
+  temp <- x
+  while (!all(temp == 0)){
+    print(temp)
+    start <- c(start,temp[1])
+    temp <- diff(temp)
+  }
+  
+  count <- 0
+  for (i in length(start):1){
+  count <- start[i] - count
+}
+  
+  return(count)
+}
+lll <- apply(dat,1,getbegin)
+sum(lll)
+
+
 
 1479039823
